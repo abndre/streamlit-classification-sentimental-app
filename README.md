cbee495f806273b5987e0bdb8ce30e986e9027eb
Andre Santos Barros da Silva <andrezio@live.com>
2023-02-09 18:39:33 -0300

    modelo apresentado no streamlit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # PIV - Streamlit Classification Dentimental App
 
 Projeto teste para uma solução serverless para um problema de dados
+
+## Web Link
+
+Acesse o link: https://abndre-streamlit-classification-sentimenta-streamlit-app-hcubbj.streamlit.app/
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,37 @@
 import streamlit as st
 import pandas as pd
-
+sentimento  = ""
 st.write("""
 # Projeto Integrador 4 - Interface Reativa
 """)
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+import joblib
+# Carrega o modelo e o vetorizador
+clf = joblib.load("model//model.pkl")
+vectorizer = joblib.load("model//vectorizer.pkl")
+
+
+def prediction(text):
+
+    # Dados de teste (texto)
+    new_text = [text]
+
+    # Transforma o novo texto em um vetor de recursos
+    new_text_vector = vectorizer.transform(new_text)
+
+    # Faz a previsão
+    prediction = clf.predict(new_text_vector)
+
+    #print("A previsão é:", prediction[0])
+    return prediction[0]
+
+title = st.text_input('Coloque o texto para avaliação de sentimento', 'Hoje foi um bom dia')
+if st.button('Predição'):
+    returno_predicao = prediction(title)
+    if returno_predicao ==0:
+        sentimento = "Negativo"
+    else:
+        sentimento = "Positivo"
+    st.write(f"A previsão é: {returno_predicao} -> {sentimento}")
+
+#st.write('A previsão é: ', title)
